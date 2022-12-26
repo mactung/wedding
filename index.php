253e9d6e92274ba0d1a2dc6c1197b7fb6ae3b6f9
MacTung <mactrantung@gmail.com>
2022-12-26 14:17:06 +0700

    update

--- index.php
+++ index.php
@@ -293,10 +293,10 @@ function getImageCdn($url)
         </section>
         <!-- end story-section -->
         <!-- start wpo-video-section -->
-        <section class="wpo-video-section">
+        <!-- <section class="wpo-video-section">
             <h2 class="hidden">some</h2>
             <a href="https://www.youtube.com/embed/G-rzE-9zHj8" class="video-btn" data-type="iframe"><i class="fi flaticon-play"></i></a>
-        </section>
+        </section> -->
         <!-- end wpo-video-section-->
         <!-- start wpo-portfolio-section -->
         <section class="wpo-portfolio-section section-padding" id="gallery">
